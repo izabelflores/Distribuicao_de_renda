0cdbe771ac1cc54334caacad322633b9b9cf2706
Izabel Flores <izabelflores9@gmail.com>
2021-11-08 18:15:46 -0300

    Divindo a base em RTB RB1 e RB2 para exportar em CSV

--- Importacao e criacao de variaveis.R
+++ Importacao e criacao de variaveis.R
@@ -4,7 +4,7 @@ library(readxl)
 library(tidyverse)
 options(scipen=999)
 
-# Importar
+#%% Importar
 
 Centil <- read_xlsx("C:/Users/izabe/Desktop/centis_2013_2020.xlsx",
                     sheet = "centil",
@@ -28,3 +28,18 @@ Centil <- Centil %>% mutate(Rendimentos_Isentos = Lucros_e_dividendos +
                               Moveis + 
                               Financeiros +
                               Outros_Bens_e_Direitos) 
+
+#%% Dividir para exportar
+
+Centil_RTB <- Centil %>% filter(Tipo_de_renda == "RTB")
+Centil_RB1 <- Centil %>% filter(Tipo_de_renda == "RB1")
+Centil_RB2 <- Centil %>% filter(Tipo_de_renda == "RB2")
+
+#%% salvando em CSV
+
+write.csv(Centil_RTB, "C:/Users/izabe/Desktop/Github/Distribuicao_de_renda/Centil_RTB.csv")
+write.csv(Centil_RB1, "C:/Users/izabe/Desktop/Github/Distribuicao_de_renda/Centil_RB1.csv")
+write.csv(Centil_RB2, "C:/Users/izabe/Desktop/Github/Distribuicao_de_renda/Centil_RB2.csv")
+
+
+
